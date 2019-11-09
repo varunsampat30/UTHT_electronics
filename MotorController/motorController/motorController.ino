84c97b5bd04ab2dd6ac5b8338c7816589a7e2d3f
Varun Sampat <varunsampat@Varuns-laptop.local>
2019-11-09 15:46:24 -0500

    Added speed function

--- MotorController/motorController/motorController.ino
+++ MotorController/motorController/motorController.ino
@@ -5,14 +5,18 @@ int motor1_pin2 = 11;
 int motor2_pin1 = 9;
 int motor2_pin2 = 10;
 
+int enable_pin = 2; 
+// this pin MUST be a pin that supports PWM.
+
 void setup() {
 //  Serial.begin(115200);
-  // put your setup code here, to run once:
   pinMode(motor1_pin1, OUTPUT);
   pinMode(motor1_pin2, OUTPUT);
 
   pinMode(motor2_pin1, OUTPUT);
   pinMode(motor2_pin2, OUTPUT);
+
+  pinMode(enable_pin, OUTPUT);
 }
 
 void moveAntiClockWise(){
@@ -36,6 +40,11 @@ void stopMotors(){
   digitalWrite(motor2_pin2, LOW);
 }
 
+// has to be between 0-255
+void setSpeed(int speed){
+  analogWrite(enable_pin, speed);
+}
+
 void loop() {
   /*
   if(Serial.available() > 0){
@@ -50,10 +59,12 @@ void loop() {
     }
   }
   */
+  setSpeed(250);
   moveAntiClockWise();
   delay(TIME_DELAY);
   stopMotors();
   delay(TIME_DELAY);
+  setSpeed(150);
   moveClockWise();
   delay(TIME_DELAY);
   stopMotors();
